e5d9d7a5fb30a9504f617f80c0bd6b8e496540de
Sergey Novikov <novikov359@gmail.com>
2016-06-26 14:25:56 +0300

    Change delta to 0.000001 for be_within matcher in specs

--- dice_spec.rb
+++ dice_spec.rb
@@ -11,9 +11,9 @@ describe Dice::Calculator do
       it 'returns correct probability' do
         expect(@calculator.probability(2, 1)).to eq(0)
         expect(@calculator.probability(2, 2)).to eq(1.0 / 36)
-        expect(@calculator.probability(3, 15)).to be_within(0.1).of(5.0 / 108)
-        expect(@calculator.probability(100, 100)).to be_within(0.1).of(1.531 * 10 ** - 78)
-        expect(@calculator.probability(300, 300)).to be_within(0.1).of(3.586 * 10 ** - 234)
+        expect(@calculator.probability(3, 15)).to be_within(0.000001).of(5.0 / 108)
+        expect(@calculator.probability(100, 100)).to be_within(0.000001).of(1.531 * 10 ** -78)
+        expect(@calculator.probability(300, 300)).to be_within(0.000001).of(3.586 * 10 ** -234)
       end
     end
   end
